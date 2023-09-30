57a8b8e01cbf22534d70a2155347858c644ff029
rizfanradya <rizfankusuma@gmail.com>
2023-09-30 12:31:50 +0700

    up

--- src/app/navbar/components/elements/hamburgerMenu.js
+++ src/app/navbar/components/elements/hamburgerMenu.js
@@ -1,28 +1,19 @@
 "use client";
-import { getStorage } from "@/utils/getStorage";
 import Image from "next/image";
 
 export default function HamburgerMenu({ onSlider }) {
-  const data = getStorage("menu.svg");
-
   const handleSlider = () => {
     onSlider();
   };
 
   return (
-    <>
-      {data ? (
-        <Image
-          className="cursor-pointer z-20 md:hidden"
-          onClick={handleSlider}
-          src={data}
-          width={30}
-          height={30}
-          alt="menu"
-        />
-      ) : (
-        <div className="animate-pulse h-7 w-7 bg-slate-600 rounded-md"></div>
-      )}
-    </>
+    <Image
+      className="cursor-pointer z-20 md:hidden"
+      onClick={handleSlider}
+      src={"menu.svg"}
+      width={30}
+      height={30}
+      alt="menu"
+    />
   );
 }
